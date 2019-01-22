d1edd212b32ef7d60f2f90f99ed915879f76dc18
galaxy yr <galaxy_yr@foxmail.com>
2019-01-22 16:24:46 +0800

    update

--- euler_sieve.cpp
+++ euler_sieve.cpp
@@ -7,17 +7,24 @@ Created Time:2019年01月22日 星期二 15时53分29秒
 #include<cstdio>
 using namespace std;
 const int maxn=1e6+10;
-int n=1e6,prime[maxn],vis[maxn],tot,mn[maxn],q[maxn];
+int n=1e6,prime[maxn],vis[maxn],tot,mn[maxn],q[maxn],phi[maxn];
 void euler_sieve()
 {
         for(int i=2;i<=n;i++)
         {
-                if(!vis[i]) { prime[++tot]=i;mn[i]=i;q[i]=1; }
+                if(!vis[i]) { prime[++tot]=i;mn[i]=i;q[i]=1;phi[i]=i-1; }
                 for(int j=1;j<=tot and prime[j]*i<=n;j++)
                 {
                         vis[prime[j]*i]=1;
                         mn[prime[j]*i]=prime[j];
-                        if(i%prime[j]==0){q[prime[j]*i]=q[i]+1;break;}
+                        q[prime[j]*i]=1;
+                        if(i%prime[j]==0)
+                        {
+                                phi[i*prime[j]]=phi[i]*prime[j];
+                                q[prime[j]*i]=q[i]+1;
+                                break;
+                        }
+                        else phi[i*prime[j]]=phi[i]*(prime[j]-1);
                 }
         }
 }
