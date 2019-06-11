19ac8cc4d389e722a755ff642cce656f062535d5
galaxy yr <galaxy_yr@foxmail.com>
2019-06-11 14:22:20 +0800

    update cantor

--- cantor.cpp
+++ cantor.cpp
@@ -1,28 +1,38 @@
 #include<iostream>
 using namespace std;
-int cantor(int *a,int n)
+const int mod=998244353;
+const int maxn=1e6+10;
+long long FAC[maxn];
+long long cantor(int *a,int n)
 {
-	static const int FAC[]={1,1,2,6,24,120,720,5040,40320,362880};//factor
-	int x=0;
-	for(int i=0;i<n;i++)
-	{
-		int smaller=0;
-		for(int j=i+1;j<n;j++)
-		{
-			if(a[j]<a[i])smaller++;
-		}
-		x+=FAC[n-i-1]*smaller;
-	}
-	return x;
+    long long x=0;
+    for(int i=0;i<n;i++)
+    {
+        int smaller=0;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])smaller++;
+        }
+        x+=FAC[n-i-1]*smaller;
+        x%=mod;
+    }
+    x++;
+    x%=mod;
+    return x;
 }
 int main()
 {
-	int* a;
-	int n;
-	cin>>n;
-	a=new int [n];
-	for(int i=0;i<n;i++)
-		cin>>a[i];
-	cout<<cantor(a,n);
-	return 0;
+    freopen("cantor.in","r",stdin);
+    freopen("cantor.out","w",stdout);
+    int* a;
+    int n;
+    cin>>n;
+    FAC[1]=1;
+    for(int i=2;i<=n;i++)
+        FAC[i]=(long long)FAC[i-1]*i%mod;
+    a=new int [n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    cout<<cantor(a,n);
+    return 0;
 }
