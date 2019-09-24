5d9046286f14767f5ce4383d192e19ea9a0d421d
galaxy yr <galaxy_yr@foxmail.com>
2019-09-24 21:51:09 +0800

    update

--- lca.cpp
+++ lca.cpp
@@ -1,32 +1,51 @@
 #include<iostream>
+#include<cstdio>
+#include<algorithm>
+
 using namespace std;
-const int maxn=5e5+100;
-const int LG=25;
-int father[maxn];
-int d[maxn];
-int f[maxn][LG];
-int head[maxn];
-int n,m,s;
+
 struct edge{
 	int to,next;
-}e[maxn];
-int cnt;
+};
+
+const int maxn=5e5+100;
+const int LG=25;
+int f[maxn][LG+1],head[maxn],n,m,s,cnt,depth[maxn];
+edge e[maxn<<1];
+
 void add(int u,int v)
 {
 	e[++cnt].next=head[u];
 	head[u]=cnt;
 	e[cnt].to=v;
 }
-void getfather(int fa)
+
+void dfs(int now)
 {
-	for(int i=head[fa];i;i=e[i].next)
-		if(e[i].to!=fa)
-		{
-			d[i]=d[fa]+1;
-			for(int j=0;j<20;j++)
-				f[i][j+1]=f[f[i][j]][j];
-		}
+    depth[now]=depth[f[now][0]]+1;
+    for(int i=1;i<=LG;i++)
+        f[now][i]=f[f[now][i-1]][i-1];
+    for(int i=head[now];i;i=e[i].next)
+        if(e[i].to!=f[now][0])
+        {
+            f[e[i].to][0]=now;
+            dfs(e[i].to);
+        }
 }
+
+int LCA(int x,int y)
+{
+    if(depth[x]<depth[y])swap(x,y);
+    for(int i=LG;i>=0;i--)
+        if(depth[f[x][i]]>=depth[y])
+            x=f[x][i];
+    if(x==y)return x;
+    for(int i=LG;i>=0;i--)
+        if(f[x][i]!=f[y][i])
+            x=f[x][i],y=f[y][i];
+    return f[x][0];
+}
+
 int main()
 {
 }
