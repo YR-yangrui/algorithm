396679878f479680848153e665a2e175367a5212
Galaxy yr <galaxy_yr@foxmail.com>
2019-11-06 22:36:21 +0800

    update

--- MillerRabin.cpp
+++ MillerRabin.cpp
@@ -24,14 +24,27 @@ struct IO{
 const int primes[]={2,3,5,7,11,13,17,19,23};
 const int SIZE=9;
 
-long long ksm(long long a,long long b)
+long long mul(long long a,long long b,long long mod)
+{
+    long long ans=0;
+    while(b)
+    {
+        if(b&1)
+            ans=(ans+a)%mod;
+        a=(a+a)%mod;
+        b>>=1;
+    }
+    return ans;
+}
+
+long long ksm(long long a,long long b,long long mod)
 {
     long long res=1;
     while(b)
     {
         if(b&1)
-            res=res*a;
-        a=a*a;
+            res=mul(res,a,mod);
+        a=mul(a,a,mod);
         b>>=1;
     }
     return res;
@@ -45,10 +58,10 @@ bool Miller_Rabin(long long x)
     for(int i=0;i<SIZE;i++)
     {
         if(x==primes[i]) return true;
-        k=ksm(primes[i],tmp);
+        k=ksm(primes[i],tmp,x);
         for(int j=1;j<=s;j++)
         {
-            nxt=k*k%x;
+            nxt=mul(k,k,x);
             if(nxt==1 && k!=1 && k!=x-1)
                 return false;
             k=nxt;
